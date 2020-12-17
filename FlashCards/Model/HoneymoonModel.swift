acf9e3929c55e4512818baf83d1e616e0bbd4b61
sxc <qiuyequ@icloud.com>
2020-12-17 12:09:54 +0800

    create a single card

---
+++ FlashCards/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  FlashCards
+//
+//  Created by Xiaochun Shen on 2020/12/17.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
