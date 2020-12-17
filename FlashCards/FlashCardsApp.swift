0e455d2c119e2297c270b64a91031d4b9e64be68
sxc <qiuyequ@icloud.com>
2020-12-17 11:11:36 +0800

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Xiaochun Shen on 2020/12/17.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
