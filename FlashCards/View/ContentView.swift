151281724d59ba71cc5fa030389ff995a3eabaeb
sxc <qiuyequ@icloud.com>
2020-12-18 09:47:56 +0800

    binding views

--- FlashCards/View/ContentView.swift
+++ FlashCards/View/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PORPERTIES
+    
+    @State var showAlert: Bool = false
+    
+    
     var body: some View {
         VStack {
             HeaderView()
@@ -16,9 +21,12 @@ struct ContentView: View {
             // fixme: add padding to the cards later on.
                 .padding()
             Spacer()
-            FooterView()
+            FooterView(showBookingAlert: $showAlert)
             
         }
+        .alert(isPresented: $showAlert, content: {
+            Alert(title: Text("SUCCESS"), message: Text("Wishing a loverly and most precious of the times together for the amazing couple."), dismissButton: .default(Text("Happy Honeymoon!")))
+        })
     }
 }
 
--- FlashCards/View/FooterView.swift
+++ FlashCards/View/FooterView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct FooterView: View {
+    // MARK: - PROPERTIES
+    @Binding var showBookingAlert: Bool
+    
     var body: some View {
         HStack {
             Image(systemName: "xmark.circle")
@@ -16,7 +19,9 @@ struct FooterView: View {
             
            
             
-            Button(action: {}
+            Button(action: {
+                self.showBookingAlert.toggle()
+            }
             ) {
                 Text("Book Destination".uppercased())
                     .font(.system(.subheadline, design: .rounded))
@@ -39,8 +44,10 @@ struct FooterView: View {
 }
 
 struct FooterView_Previews: PreviewProvider {
+    @State static var showAlert: Bool = false
+    
     static var previews: some View {
-        FooterView()
+        FooterView(showBookingAlert: $showAlert)
             .previewLayout(.fixed(width: 375, height: 80))
     }
 }
